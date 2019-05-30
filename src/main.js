ed9b038135d304daf4dc5fc3315fe7d9983213e4
syjung <syjung@maxst.com>
2019-05-30 15:28:20 +0900

    불필요한 파일 삭제 및 코드 정리

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { ClientTable } from "vue-tables-2";
-// import daterangepicker from 'daterangepicker';
-//
-// window.moment = require('moment');
 
 
 Vue.config.productionTip = false
@@ -15,9 +12,7 @@ let useVuex = false;
 let theme = "bootstrap4";
 let template = "default";
 Vue.use(ClientTable, options, useVuex, theme, template);
-// Vue.use(daterangepicker);
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
--- src/pages/tables/Tables.vue
+++ src/pages/tables/Tables.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tables_wrapper">
     <div class="tables">
-      <h1>{{ msg }}</h1>
+      <h1>{{ title }}</h1>
       <v-client-table :data="tableData" :columns="columns" :options="options">
         <a slot="email" slot-scope="props" :href="`mailto:${props.row.email}`">
           {{props.row.email}}
@@ -16,38 +16,36 @@
     name: 'Tables',
     data () {
       return {
-        msg: 'Sample Table',
-        columns: ["id", "name", "age", "email", "isTrue"],
-        tableData: this.getData(),
+        title: 'vue-tables-2 Example',
+        columns: ["id", "name", "age", "email", "isTrue", "image"], // 테이블 컬럼
+        tableData: this.getData(), // 테이블 데이터
         options: {
-          filterByColumn: true,
-          perPage: 10,
-          listColumns: {
+          filterByColumn: true, // 컬럼별 검색 기능
+          perPage: 10, // 한 페이지에 몇 개 보여줄 지 설정
+          listColumns: { // 드롭다운 선택 필터
             isTrue: [
               { id: 'true', text: 'O' },
               { id: 'false', text: 'X' }
             ],
           },
-          headings: {
+          headings: { // thead 이름 set
             id: 'User Id',
             name: 'User Name',
             age: 'User Age',
             email: 'User Email',
-            isTrue: 'User isTrue'
+            isTrue: 'User isTrue',
+            birth_date: 'Birth Date'
           },
-          sortable: ['id', 'name', 'age', 'isTrue'],
-          // 검색 filter에 포함시킬 데이터
-          filterable: ['id', 'name', 'age', 'email', 'isTrue'],
-          texts: {
+          sortable: ['id', 'name', 'age', 'isTrue'], // 정렬 기준에 포함시킬 데이터
+          filterable: ['id', 'name', 'age', 'email', 'isTrue'], // 검색 필터에 포함시킬 데이터
+          texts: { // 텍스트 대치
             filterPlaceholder: 'Search',
-            filter: "Search",
+            filter: "Search :",
             filterBy: 'Search {column}',
-            count: "",
+            count: "", // 기본값 예시 : Showing 1 to 10 of 250 records
           },
-          pagination: { chunk: 5, dropdown: false },
-          datepickerOptions: {
-            showDropdowns: true
-          },
+          pagination: { chunk: 5, dropdown: false }, // 페이지네이션 세팅
+          // compileTemplates: true,
         }
       }
     },
@@ -67,7 +65,7 @@
             'email': `sample${i}@example.com`,
             'age': `${this.getRandomInt(10, 100)}`,
             'isTrue': `${i % 2 == 0 ? true : false}`,
-            // 'birth_date': `${this.randomDate(new Date(1925, 0, 1), new Date(2012, 0, 1))}`
+            'image': `<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Mooncake1.jpg/300px-Mooncake1.jpg'/>`
           })
         }
         return arr;
@@ -75,9 +73,6 @@
       // min 포함, max 불포함
       getRandomInt (min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
-      },
-      randomDate(start, end) {
-        return this.moment(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
       }
     },
     components: {
