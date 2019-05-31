c362fa918f7dfef7f85fa089225306071bb146ee
syjung <syjung@maxst.com>
2019-05-31 10:30:45 +0900

    유저 생성 기능 추가

---
+++ src/components/create/Create.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="create_wrapper" v-show="show">
+    <div class="create">
+      <div class="title">
+        <h2>{{ title }}</h2>
+        <input type="text" placeholder="name" v-model="user.name">
+        <input type="text" placeholder="age" v-model="user.age">
+        <input type="text" placeholder="email" v-model="user.email">
+        <input type="text" placeholder="explanation" v-model="user.explanation">
+        <div>
+          isTrue :
+          <input type='radio' value='true' id="true" v-model="user.isTrue"/>
+          <label for="true">O</label>
+          <input type='radio' value='false' id="false" v-model="user.isTrue"/>
+          <label for="false">X</label>
+        </div>
+      </div>
+
+      <div class="buttons">
+        <button class="btn blue-button" @click="onSave">저장</button>
+        <button class="button grey-button" @click="closePopup">취소</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Create',
+    props: ['show'],
+    data () {
+      return {
+        title : 'Create',
+        user: {
+          isTrue: true,
+          name: '',
+          age: '',
+          email: '',
+          explanation: '',
+          image: 'https://www.netfort.com/assets/user.png',
+        }
+      }
+    },
+    watch: {
+      show (newValue, oldValue) {
+        if (newValue === true) {
+          this.user.isTrue = true;
+          this.user.name = '';
+          this.user.age = '';
+          this.user.email = '';
+          this.user.explanation = '';
+          this.user.image = 'https://www.netfort.com/assets/user.png';
+        }
+      }
+    },
+    computed: {
+    },
+    mounted () {
+    },
+    methods: {
+      closePopup () {
+        this.$emit('close');
+      },
+      onSave () {
+        this.$emit('save', this.user);
+      }
+    },
+    components: {
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "Create.scss";
+</style>
--- src/pages/tables/Tables.vue
+++ src/pages/tables/Tables.vue
@@ -2,29 +2,32 @@
   <div class="tables_wrapper">
     <div class="tables">
       <h1>{{ title }}</h1>
-      <v-client-table :data="tableData" :columns="columns" :options="options">
+      <button class="blue-button" @click="isOpenCreatePopup = true">등록하기</button>
+      <v-client-table :data="userList" :columns="columns" :options="options">
         <a slot="email" slot-scope="props" :href="`mailto:${props.row.email}`">
           {{props.row.email}}
         </a>
         <template slot="image" scope="props">
           <img :src="props.row.image" alt="" width="50" height="50">
         </template>
-        <template slot="child_row" slot-scope="props">
-          The link to {{props.row.name}} is <a :href="props.row.url" target="_blank">{{props.row.url}}</a>
+        <template slot="child_row" scope="props">
+          {{props.row.name}}의 부가 설명 : <a :href="props.row.url" target="_blank">{{props.row.explanation}}</a>
         </template>
       </v-client-table>
     </div>
+    <create-component :show="isOpenCreatePopup" @save="saveUser" @close="closeCreatePopup"></create-component>
   </div>
 </template>
 
 <script>
+  import CreateComponent from '../../components/create/Create'
+
   export default {
     name: 'Tables',
     data () {
       return {
         title: 'JESSIE\'s vue-tables-2 Example',
         columns: ["id", "image", "name", "age", "email", "isTrue" ], // 테이블 컬럼
-        tableData: this.getData(), // 테이블 데이터
         options: {
           filterByColumn: true, // 컬럼별 검색 기능
           perPage: 10, // 한 페이지에 몇 개 보여줄 지 설정
@@ -40,7 +43,7 @@
             age: 'User Age',
             email: 'User Email',
             isTrue: 'User isTrue',
-            image: 'User Image'
+            image: 'User Image',
           },
           sortable: ['id', 'name', 'age', 'isTrue'], // 정렬 기준에 포함시킬 데이터
           filterable: ['id', 'name', 'age', 'email', 'isTrue'], // 검색 필터에 포함시킬 데이터
@@ -54,7 +57,10 @@
           // compileTemplates: true,
           fields: ['__slot:image'],
           highlightMatches: true,
-        }
+        },
+        userList: [],
+        isOpenCreatePopup: false,
+        newUser: {},
       }
     },
     watch: {
@@ -62,29 +68,38 @@
     computed: {
     },
     mounted () {
+      this.getData();
     },
     methods: {
       getData () {
-        const arr = [];
-        for (let i = 0; i < 250; i++) {
-          arr.push({
+        for (let i = 0; i < 9; i++) {
+          this.userList.push({
             'id': i,
             'name': `sample${i}`,
             'email': `sample${i}@example.com`,
             'age': `${this.getRandomInt(10, 100)}`,
             'isTrue': `${i % 2 == 0 ? true : false}`,
             'image': 'https://www.netfort.com/assets/user.png',
-            'url': 'http://maxst.com',
+            'explanation': 'http://maxst.com',
           })
         }
-        return arr;
       },
       // min 포함, max 불포함
       getRandomInt (min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
+      },
+      closeCreatePopup () {
+        this.isOpenCreatePopup = false;
+      },
+      saveUser (user) {
+        this.newUser = JSON.parse(JSON.stringify(user));
+        this.newUser.id = this.userList[this.userList.length - 1].id + 1;
+        this.userList.push(this.newUser);
+        this.isOpenCreatePopup = false;
       }
     },
     components: {
+      CreateComponent,
     }
   }
 </script>
